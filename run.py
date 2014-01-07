81b6ea22d5de9f2e113f76e67d875a7f398bf398
Sean Cascketta <scascketta@gmail.com>
2014-01-07 14:54:22 -0600

    Forgot to change app name reference in run

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from dochost import app
+from historyemergent import app
 # Comment out the line below if you are using something other than the built-in
 # Flask development server
 app.run(host='127.0.0.1', port=5000, debug=True)
